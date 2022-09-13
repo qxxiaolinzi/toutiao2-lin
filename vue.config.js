888e9cd5a1f8bbddc5b6359200abcd3271605e8e
lin <2284309229@qq.com>
2022-09-13 20:21:21 +0800

    项目初始化完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { join } = require('path')
+const cpns = join(__dirname, 'src/components')
+const views = join(__dirname, 'src/views')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,
+  transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
